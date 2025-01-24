a800e55ac9a39ec84f16c958dc920d4a860e4abc
raunak-garhwal <raunakgarhwal2003@gmail.com>
2025-01-25 02:43:47 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/v1': {
-      target: 'https://v0-new-project-lumps00hldq.vercel.app/',
-      changeOrigin: true,
-      secure: false,
-    },
-  },
 })
