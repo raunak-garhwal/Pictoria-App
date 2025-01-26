ffaa641ac62b58c4803cdc2bdf6701fb81433147
raunak-garhwal <raunakgarhwal2003@gmail.com>
2025-01-26 20:47:28 +0530

    updates

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import favicon from '../../assets/favicon.png'
 
 function Logo({width}) {
   return (
     <div>
       <img
       className='h-18 w-24'
-      src="favicon.png"
+      src={favicon}
       alt="logo"
       style={{width: width}}
       />
