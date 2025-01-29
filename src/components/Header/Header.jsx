128e437d69b63a174e8dc85c293cd5946930ea7f
raunak-garhwal <raunakgarhwal2003@gmail.com>
2025-01-29 23:42:14 +0530

    minor updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -38,17 +38,17 @@ function Header() {
   return (
     <header className='py-2 shadow border border-gray-600 rounded-xl'>
       <Container>
-        <nav className='flex items-center'>
+        <nav className='flex flex-col items-center justify-center sm:flex-row sm:justify-between'>
           <div className='ml-2'>
             <Link to='/'>
               <Logo/>
             </Link>
           </div>
-          <ul className='flex ml-auto text-slate-200 text-lg font-sans font-semibold'>
+          <ul className='flex flex-col sm:flex-row justify-center items-center w-full sm:w-auto text-slate-200 text-lg font-sans font-semibold'>
             {navItems.map((item) => (
               item.active ? (
                 <li key={item.name}>
-                  <button onClick={() => navigate(item.slug)}className='inline-block px-5 py-2 duration-200 hover:bg-gray-800 rounded-full'>
+                  <button onClick={() => navigate(item.slug)} className='inline-block px-5 py-2 duration-200 hover:bg-gray-800 rounded-full'>
                     {item.name}
                   </button>
                 </li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,15 +16,15 @@ function Home() {
     if (posts.length === 0) {
         return (
             <div className="w-full py-8 mt-4 text-center">
-                <Container>
-                    <div className="flex flex-wrap">
-                        <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold">
-                                Login to read posts
-                            </h1>
-                        </div>
-                    </div>
-                </Container>
+            <Container>
+                <div className="flex flex-wrap">
+                <div className="p-2 w-full">
+                    <h1 className="text-2xl font-bold">
+                    Login to read posts
+                    </h1>
+                </div>
+                </div>
+            </Container>
             </div>
         )
     }
@@ -33,7 +33,7 @@ function Home() {
             <Container>
                 <div className='flex flex-wrap'>
                     {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
+                        <div key={post.$id} className='p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4'>
                             <PostCard {...post} />
                         </div>
                     ))}
