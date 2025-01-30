aff84f8e204c3d1d13145758a47e329b6ced2e3e
raunak-garhwal <raunakgarhwal2003@gmail.com>
2025-01-30 23:35:09 +0530

    final updates

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -69,55 +69,67 @@ export default function PostForm({ post }) {
         });
 
         return () => subscription.unsubscribe();
-    }, [watch, slugTransform, setValue]);
+        }, [watch, slugTransform, setValue]);
 
-    return (
-        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
-            <div className="w-2/3 px-2">
+        return (
+            <form onSubmit={handleSubmit(submit)}>
+            <div className="flex flex-col md:flex-row justify-center">
+                <div className="w-full md:w-2/3 px-2 flex flex-col items-center">
                 <Input
                     label="Title :"
                     placeholder="Title"
-                    className="mb-4"
+                    className="mb-4 w-full"
                     {...register("title", { required: true })}
                 />
                 <Input
                     label="Slug :"
                     placeholder="Slug"
-                    className="mb-4"
+                    className="mb-4 w-full"
                     {...register("slug", { required: true })}
                     onInput={(e) => {
-                        setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                    setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
                     }}
                 />
-                <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
-            </div>
-            <div className="w-1/3 px-2">
+                <RTE 
+                    label="Content :" 
+                    name="content" 
+                    control={control} 
+                    defaultValue={getValues("content")} 
+                    className="w-full"
+                />
+                </div>
+                <div className="w-full md:w-1/3 px-2 mt-4 md:mt-0 flex flex-col items-center">
                 <Input
                     label="Featured Image :"
                     type="file"
-                    className="mb-4"
+                    className="mb-4 w-full"
                     accept="image/png, image/jpg, image/jpeg, image/gif"
                     {...register("image", { required: !post })}
                 />
                 {post && (
-                    <div className="w-full mb-4">
-                        <img
-                            src={appwriteService.getFilePreview(post.featuredImage)}
-                            alt={post.title}
-                            className="rounded-lg"
-                        />
+                    <div className="w-full mb-4 flex justify-center">
+                    <img
+                        src={appwriteService.getFilePreview(post.featuredImage)}
+                        alt={post.title}
+                        className="rounded-lg"
+                    />
                     </div>
                 )}
                 <Select
                     options={["active", "inactive"]}
                     label="Status:"
-                    className="mb-4"
+                    className="mb-4 w-full"
                     {...register("status", { required: true })}
                 />
-                <Button type="submit" bgColor={post ? "bg-green-500 hover:bg-green-800" : undefined} className="w-full ">
+                <Button 
+                    type="submit" 
+                    bgColor={post ? "bg-green-500 hover:bg-green-800" : undefined} 
+                    className="w-full"
+                >
                     {post ? "Update" : "Submit"}
                 </Button>
+                </div>
             </div>
-        </form>
-    );
+            </form>
+        );
 }
