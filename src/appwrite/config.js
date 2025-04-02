0a419f52de284fc1f016692e103321d2b81a6bb7
raunak-garhwal <raunakgarhwal2003@gmail.com>
2025-04-03 01:28:50 +0530

    fixed the image preview bug

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -122,7 +122,7 @@ export class Service{
     }
 
     getFilePreview(fileId) {
-        return this.bucket.getFilePreview(
+        return this.bucket.getFileView(
             conf.appwriteBucketId,
             fileId
         )
